3b1d413797dacdd26cb9f803763d53049092d2c4
Lin Zhou <lzhou58@my.bcit.ca>
2017-11-23 11:02:21 -0800

    migration
    autentication migration

--- KoalaCalendar/Data/Migrations/20171123185557_AddUserAuthentication.Designer.cs
+++ KoalaCalendar/Data/Migrations/20171123185557_AddUserAuthentication.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace KoalaCalendar.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171123185557_AddUserAuthentication")]
+    partial class AddUserAuthentication
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
