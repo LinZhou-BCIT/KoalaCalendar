7a58eb2d8f67c0c9bfa8b78ae0adb486c8730f0f
Lin Zhou <lzhou58@my.bcit.ca>
2017-11-29 09:33:06 -0800

    simple calendarVM

--- KoalaCalendar/Models/ViewModels/CalendarVM.cs
+++ KoalaCalendar/Models/ViewModels/CalendarVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,9 @@ namespace KoalaCalendar.Models.ViewModels
 {
     public class CalendarVM
     {
+        [DisplayName("Title")]
+        public string Title { get; set; }
+        [DisplayName("Key")]
+        public string Key { get; set; }
     }
 }
