7bd4b4e6a148d372121586a28e08e05ac1f9ba4a
Lin Zhou <lzhou58@my.bcit.ca>
2017-11-29 14:06:11 -0800

    minor fix of imports

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { CalendarListComponentComponent } from './components/calendar-list-component/calendar-list-component.component';
-import { CalendarCreateComponentComponent } from './components/calendar-create-component/calendar-create-component.component';
 import { CalendarCreateComponent } from './components/calendar-create/calendar-create.component';
 import { CalendarListComponent } from './components/calendar-list/calendar-list.component';
 import { CalendarSubComponent } from './components/calendar-sub/calendar-sub.component';
@@ -14,8 +12,6 @@ import { EventCreateComponent } from './components/event-create/event-create.com
 @NgModule({
   declarations: [
     AppComponent,
-    CalendarListComponentComponent,
-    CalendarCreateComponentComponent,
     CalendarCreateComponent,
     CalendarListComponent,
     CalendarSubComponent,
