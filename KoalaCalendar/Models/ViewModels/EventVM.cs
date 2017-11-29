2039a9609fe95cf6b8d76a93ac7a6b162aefd249
Lin Zhou <lzhou58@my.bcit.ca>
2017-11-29 09:28:06 -0800

    empty VMs

---
+++ KoalaCalendar/Models/ViewModels/EventVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KoalaCalendar.Models.ViewModels
+{
+    public class EventVM
+    {
+    }
+}
