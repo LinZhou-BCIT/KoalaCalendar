c6355e4b01c413e6b92337fe88db7d899113ac6b
Dak <32491405+DakKhuza@users.noreply.github.com>
2018-02-08 14:20:16 -0800

    Added JSON Parsing placeholder class

---
+++ APIServer/JSONParsing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIServer
+{
+    public class JSONParsing
+    {
+
+        //Todo Add JSON Parser
+
+    }
+}
