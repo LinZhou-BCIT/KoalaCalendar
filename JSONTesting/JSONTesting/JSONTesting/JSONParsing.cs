ffff0c49c595df1b25a1a07365a45bd735ae1eeb
Dak <32491405+DakKhuza@users.noreply.github.com>
2018-02-08 17:10:31 -0800

    Working on JSON

--- JSONTesting/JSONTesting/JSONTesting/JSONParsing.cs
+++ JSONTesting/JSONTesting/JSONTesting/JSONParsing.cs
@@ -1,22 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Web;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
 
 namespace JSONTesting
 {
     class JSONParsing
     {
+        internal class CalendarEvent
+        {
+            internal int date;
 
-        private string testString = 
-            "{'type':'C_GET','CK':'1','Auth':'<something to do with auth>'," +
-            "'Event_ID':{'EVENT_DAY':'20','EVENT_NAME':'test','EVENT_DESC':'generic','EVENT_START':'datetime','EVENT_END':'datetime'}}";
+            internal string name;
 
+            internal string description;
+
+            internal string eventStart;
+
+            internal string eventEnd;
+        }
+
+        internal class CalendarEventList
+        {
+            internal CalendarEvent calenderEvents;
+        }
+
+        [DataContract]
+        internal class CalendarData
+        {
+            [DataMember]
+            internal string CallType;
+
+            [DataMember]
+            internal int CalKey;
+
+            [DataMember]
+            internal string Auth;
+
+            [DataMember]
+            internal CalendarEventList EventList;
+        }
+
+        public void Test()
+        {
+            CalendarData c = new CalendarData();
+            c.CallType = "C_GET";
+            c.CalKey = 1;
+            c.Auth = "aaaa";
+            //c.Events = "";
+        }
 
         public IDictionary<string, string> Parse(string jsonString)
         {
             IDictionary<string, string> JsonMap = new Dictionary<string, string>();
-            Json.Decode
+
+
 
 
             return JsonMap;
--- JSONTesting/JSONTesting/JSONTesting/Program.cs
+++ JSONTesting/JSONTesting/JSONTesting/Program.cs
@@ -6,7 +6,7 @@ namespace JSONTesting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
