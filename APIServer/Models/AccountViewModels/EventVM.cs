a680d072849d337ee5d818ec8b573c136c1bced1
cchaa22 <wtumtaweetikul@my.bcit.ca>
2018-02-13 10:03:15 -0800

    Created EventVM

---
+++ APIServer/Models/AccountViewModels/EventVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIServer.Models.AccountViewModels
+{
+    public class EventVM
+    {
+        public string EventID { get; set; }
+        public string EventName { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+    }
+}
