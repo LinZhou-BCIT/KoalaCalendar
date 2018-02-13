051d7f5d4bfe3cc24f124f2de23d7307facec614
cchaa22 <wtumtaweetikul@my.bcit.ca>
2018-02-13 11:53:40 -0800

    Added EventRequestVM

---
+++ APIServer/Models/CalendarViewModels/EventRequestVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIServer.Models.CalendarViewModels
+{
+    public class EventRequestVM
+    {
+        public List<string> CalendarIDs { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
