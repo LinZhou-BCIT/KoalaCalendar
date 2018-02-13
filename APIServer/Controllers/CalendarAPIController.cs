a846c2a172f1ab47cd28d7e8743427ad81a9a31b
cchaa22 <wtumtaweetikul@my.bcit.ca>
2018-02-13 11:43:08 -0800

    Edited CalendarAPIController
    created calendarVM, calendarRepo

--- APIServer/Controllers/CalendarAPIController.cs
+++ APIServer/Controllers/CalendarAPIController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using APIServer.Models.CalendarViewModels;
+using APIServer.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,7 +12,72 @@ namespace APIServer.Controllers
 {
     [Produces("application/json")]
     [Route("api/CalendarAPI")]
-    public class CalendarAPIController : Controller
+    [Authorize]
+    public class CalendarAPI : Controller
     {
+        private readonly CalendarRepo _calendarRepo;
+
+        //injection
+        public CalendarAPI(
+        CalendarRepo calendarRepo)
+        {
+            _calendarRepo = calendarRepo;
+        }
+
+        [Authorize (Roles = "Professor")]
+        [HttpPost]
+        public async Task<object> AddCalendar([FromBody] AddCalendarVM model)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<object> SubscribeCalendar(string accesCode)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<object> GetCalendar(string searchInput)
+        {
+            var listOfCalendars = await _calendarRepo.GetAllCalendars();
+
+            if (searchInput == null)
+            {
+                return listOfCalendars;
+            }
+            
+            return _calendarRepo.SearchCalendar(searchInput);
+        }
+
+        [HttpPost]
+        public async Task<object> ViewCalendar()
+        {
+            return View();
+        }
+
+        [HttpDelete]
+        public async Task<object> DeleteCalendar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddEvent()
+        {
+            return View();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateEvent()
+        {
+            return View();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteEvent()
+        {
+            return View();
+        }
     }
 }
---
+++ APIServer/Repositories/CalendarRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIServer.Repositories
+{
+    public class CalendarRepo
+    {
+        public async Task<IEnumerable<string>> GetAllCalendars()
+        {
+            return null;
+        }
+
+        public async Task<IEnumerable<string>> SearchCalendar(string searchInput)
+        {
+            return null;
+        }
+    }
+}
