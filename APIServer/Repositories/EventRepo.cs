19309699518d29069d6024cefb0b79f930f5b064
cchaa22 <wtumtaweetikul@my.bcit.ca>
2018-02-13 10:59:59 -0800

    Added CalendarAPIController

--- APIServer/Repositories/EventRepo.cs
+++ APIServer/Repositories/EventRepo.cs
@@ -7,6 +7,17 @@ namespace APIServer.Repositories
 {
     public class EventRepo
     {
+        // list of events
+        //GetAllEvents
 
+        //Create Event
+        // pass in calendarID as a parameter
+
+        //Update Event
+
+        //Delete Event
+
+        //controller verify that userID = ownerID
+        // check if user is subscribe to that calendar
     }
 }
