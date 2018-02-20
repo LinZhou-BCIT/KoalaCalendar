5ed8cb48c733534092468acfb94d813505f4ed8e
Dak <32491405+DakKhuza@users.noreply.github.com>
2018-02-20 12:02:38 -0800

    Updated CalendarData Class file, update JSON format file + Started working on methods

--- JSONTesting/JSONTesting/JSONTesting/CalendarData.cs
+++ JSONTesting/JSONTesting/JSONTesting/CalendarData.cs
@@ -12,13 +12,29 @@ namespace CalendarData
         public string EVENT_DESC { get; set; }
         public string EVENT_START { get; set; }
         public string EVENT_END { get; set; }
+        public CalendarEvent(int e_id, int e_day, string e_name, string e_desc, string e_start, string e_end)
+        {
+            EVENT_ID = e_id;
+            EVENT_DAY = e_day;
+            EVENT_NAME = e_name;
+            EVENT_DESC = e_desc;
+            EVENT_START = e_start;
+            EVENT_END = e_end;
+        }
     }
 
     public class Calendar
     {
-        public string TYPE { get; set; }
-        public string CALENDAR_KEY { get; set; }
-        public string AUTH { get; set; }
-        public CalendarEvent[] CalendarEvents { get; set; }
+        public string TYPE { get; set; } //The type of action. C_GET, C_SET, C_SEND
+        public int CALENDAR_KEY { get; set; } //The id for calendar.
+        public string AUTH { get; set; } //Some sort authentication token.
+        public List<CalendarEvent> CalendarEvents { get; set; } //Array of CalendarEvent objects.
+        public Calendar(string c_type, int c_key, string c_auth, List<CalendarEvent> c_events)
+        {
+            TYPE = c_type;
+            CALENDAR_KEY = c_key;
+            AUTH = c_auth;
+            CalendarEvents = c_events;
+        }
     }
 }
--- JSONTesting/JSONTesting/JSONTesting/JSONParsing.cs
+++ JSONTesting/JSONTesting/JSONTesting/JSONParsing.cs
@@ -37,8 +37,30 @@ namespace CalendarJson
 		]
 }";
             Calendar test = JsonConvert.DeserializeObject<Calendar>(testString);
+
+            Console.WriteLine(test.CalendarEvents.ToArray()[0].EVENT_DAY);
+            List<CalendarEvent> eventList = new List<CalendarEvent>();
+
+            CalendarEvent calEv0 = new CalendarEvent(1,2,"Generic Name","Generic Description","00","00");
+
+            CalendarEvent calEv1 = new CalendarEvent(2,3,"Name Generic", "Description Generic","00","00");
+
+            eventList.Add(calEv0);
+            eventList.Add(calEv1);
+            createCal("C_GET",1,"auth",eventList);
+        }
+
+        public void createCal(string type, int id, string auth, List<CalendarEvent> calEvents)
+        {
+            Calendar cal = new Calendar(type, id, auth, calEvents);
             
-            Console.WriteLine(test.CalendarEvents.Length);
+            Console.WriteLine(JsonConvert.SerializeObject(cal));
+        }
+
+        public void createJsonAndSend(Calendar cal)
+        {
+            String output = JsonConvert.SerializeObject(cal);
+            Console.WriteLine(cal);
         }
     }
 }
