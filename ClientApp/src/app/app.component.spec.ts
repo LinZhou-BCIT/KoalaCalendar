d46affb52c4118211451c29c81fcea3ebdb4725f
cchaa22 <wtumtaweetikul@my.bcit.ca>
2018-03-27 11:37:27 -0700

    Added calendar on AppComponent

--- ClientApp/src/app/app.component.spec.ts
+++ ClientApp/src/app/app.component.spec.ts
@@ -1,6 +1,11 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import * as moment from 'moment';
+import { AppComponent, CalendarDate } from './app.component';
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -8,6 +13,92 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
+  describe('Select date', () => {
+    it ('should emit date when selected', () => {
+      let selectedDate: CalendarDate = {
+        selected: false,
+        today: false,
+        mDate: moment()
+      };
+      component.onSelectDate.subscribe(x => selectedDate = x);
+      component.selectDate(selectedDate);
+      const isSame = moment(selectedDate.mDate).isSame(moment(selectedDate.mDate), 'day');
+      expect(isSame).toBeTruthy();
+    });
+  });
+
+  describe('Calendar grid generation', () => {
+
+    it('should generate 6 weeks', () => {
+      component.generateCalendar();
+      fixture.detectChanges();
+      expect(component.weeks.length).toEqual(6);
+    });
+
+    it('should generate Feb 2017 correctly', () => {
+      component.currentDate = moment('2017-02-05');
+      component.generateCalendar();
+      fixture.detectChanges();
+      expect(component.weeks[0][0].mDate.date()).toEqual(29);
+      expect(component.weeks[5][6].mDate.date()).toEqual(11);
+    });
+  });
+
+  describe('Year navigation', () => {
+    it('should go forward 1 year', () => {
+      component.currentDate = moment('2017-09-10');
+      fixture.detectChanges();
+      component.nextYear();
+      fixture.detectChanges();
+      expect(component.currentDate.year()).toEqual(2018);
+    });
+    it('should go backward 1 year', () => {
+      component.currentDate = moment('2017-09-10');
+      fixture.detectChanges();
+      component.prevYear();
+      fixture.detectChanges();
+      expect(component.currentDate.year()).toEqual(2016);
+    });
+  });
+
+  describe('Month navigation', () => {
+    it('should go forward 1', () => {
+      component.currentDate = moment('2017-09-10');
+      fixture.detectChanges();
+      component.nextMonth();
+      fixture.detectChanges();
+      expect(component.currentDate.month()).toEqual(9);
+    });
+    it('should go backward 1', () => {
+      component.currentDate = moment('2017-09-10');
+      fixture.detectChanges();
+      component.prevMonth();
+      fixture.detectChanges();
+      expect(component.currentDate.month()).toEqual(7);
+    });
+    it('should go to january', () => {
+      component.currentDate = moment('2017-09-10');
+      fixture.detectChanges();
+      component.firstMonth();
+      fixture.detectChanges();
+      expect(component.currentDate.month()).toEqual(0);
+    });
+    it('should go to december', () => {
+      component.currentDate = moment('2017-09-10');
+      fixture.detectChanges();
+      component.lastMonth();
+      fixture.detectChanges();
+      expect(component.currentDate.month()).toEqual(11);
+    });
+  });
+
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
@@ -25,3 +116,6 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
+
+
+
