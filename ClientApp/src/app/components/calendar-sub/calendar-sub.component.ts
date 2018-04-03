a527a61f4d75ac21c44e011ba250c9f9b5c7beef
Lin Zhou <lzhou58@my.bcit.ca>
2018-04-03 14:32:22 -0700

    basic functionality on calendar-create component

--- ClientApp/src/app/components/calendar-sub/calendar-sub.component.ts
+++ ClientApp/src/app/components/calendar-sub/calendar-sub.component.ts
@@ -18,12 +18,8 @@ export class CalendarSubComponent implements OnInit {
   subscribe() {
     this.calendarService.subToCalendar(this.accessCode).subscribe(
       result => {
-        if(result['success']) {
-          // process the message from server
-          this.router.navigate(['/calendar']);
-        } else {
-          // process and display the message from server
-        }
+        // process the message from server
+        this.router.navigate(['/calendar']);
       },
       error => {
         // process and display the message from server?
